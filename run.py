5f4742c5e2e30e75ca64bcd35d2a5744d7b1dca0
SherylWaga <42872391+SherylWaga@users.noreply.github.com>
2018-12-10 19:33:44 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from app.db_config import create_tables
 
 from app import create_app
-# config_name = os.getenvgit add ('APP_SETTINGS') #config_name="development"
+
 app = create_app()
 
 if __name__ == '__main__':
