a6e3809446875b110020a0c23e47e7c1b4f0a0a1
SherylWaga <sherylwaga@gmail.com>
2018-12-10 19:11:47 +0300

    [Bug #162549166]Enable the tables to be created on running application

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import os
+from app.db_config import create_tables
 
 from app import create_app
-# config_name = os.getenv('APP_SETTINGS') #config_name="development"
+# config_name = os.getenvgit add ('APP_SETTINGS') #config_name="development"
 app = create_app()
 
 if __name__ == '__main__':
-
+    create_tables
     app.run()
     
