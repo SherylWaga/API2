d273f194e76a87ed9df6f4f54f4cc26c4c590ce6
SherylWaga <sherylwaga@gmail.com>
2018-12-12 00:01:44 +0300

    [Finish  #162536748]Finish user-login

--- app/db_config.py
+++ app/db_config.py
@@ -1,42 +1,41 @@
 import psycopg2
 import os
 
-url="dbname='ireporter' host='localhost' port='5432' user='postgres' password='pycoders'"
-test_url ="dbname='ireporter_test' host='localhost' port='5432' user='postgres' password='pycoders'"
+url = os.getenv('DATABASE_URL')
+test_url = os.getenv('TESTDATABASE_URL')
 
-def connection (url):
-    conn=psycopg2.connect(url)
+
+def connection(url):
+    conn = psycopg2.connect(url)
     return conn
+
 
 def init_db():
-    conn=connection(url)
+    conn = connection(url)
     return conn
+
 
 def create_tables():
     users = """CREATE TABLE IF NOT EXISTS users(
     id serial PRIMARY KEY NOT NULL,
-    firstname char(30) NOT NULL,
-    lastname char(30)NOT NULL,
-    email char(30) NOT NULL,
-    phonenumber char (20) NOT NULL,
-    username char(40) NOT NULL,
-    password char(40)NOT NULL,
+    firstname VARCHAR(191) NOT NULL,
+    lastname VARCHAR(130) NOT NULL,
+    email VARCHAR(130) NOT NULL,
+    phonenumber VARCHAR(120) NOT NULL,
+    username VARCHAR(140) NOT NULL,
+    password VARCHAR(140)NOT NULL,
     registered timestamp with time zone DEFAULT('now'::text)::date NOT NULL,
-    isadmin char(40)  DEFAULT 'false' NOT NULL)"""
+    isadmin VARCHAR(130)  DEFAULT 'false' NOT NULL)"""
 
-    queries=[users]
+    queries = [users]
 
-    conn=connection(url)
-    cur=conn.cursor()
-    
-   
+    conn = connection(url)
+    cur = conn.cursor()
     for query in queries:
         cur.execute(query)
     conn.commit()
     return queries
 
+
 def destroy_table():
     pass
-
-
-    
