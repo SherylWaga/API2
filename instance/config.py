02d844747937e12a80e4564dc8e08a222e562d1b
SherylWaga <sherylwaga@gmail.com>
2018-12-11 23:31:26 +0300

    [Feature #162536748]Import url from .env

--- instance/config.py
+++ instance/config.py
@@ -1,22 +1,29 @@
+import os
+
+
 class Config(object):
     """
     Common configurations
     """
 
+
 class DevelopmentConfig(Config):
     """Development configurations"""
-    db_url = "'ireporter' host='localhost' port='5432' user='postgres' password='pycoders'"
+    url = os.getenv('DATABASE_URL')
     DEBUG = True
     
+
 class ProductionConfig(Config):
     """Production configurations"""
     DEBUG = False
+
 
 class TestingConfig(Config):
     """Testing configuration, with test database."""
     test_url ="dbname='ireporter_test' host='localhost' port='5432' user='postgres' password='pycoders'"
     TESTING = True
     DEBUG = True
+    test_url = os.getenv('TESTDATABASE_URL')
 
 app_config = {
     'development': DevelopmentConfig,
