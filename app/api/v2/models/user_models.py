87b5304c44c46b8d5830a3bb5fedee5eb3a15dd1
SherylWaga <sherylwaga@gmail.com>
2018-12-11 23:59:32 +0300

    [Feature #162536748]start user-login

--- app/api/v2/models/user_models.py
+++ app/api/v2/models/user_models.py
@@ -1,29 +1,51 @@
-from flask import Flask
-#local imports
-from  ....db_config import init_db
-app =Flask(__name__)
-app.config['SECRET_KEY']="shewags"
+from flask import Flask, request
+# local imports
+
+from ....db_config import init_db
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "shewags"
+
 
 class users():
     def __init__(self):
-        self.db=init_db()
-    
-    def save_user(self,firstname,lastname,email,phonenumber,username,password):
+        self.db = init_db()
+
+    def save_user(self, firstname, lastname, email, phonenumber, username, password):
        
-         users = {
-              'firstname':firstname,
-              'lastname':lastname,
-              'email':email,
-              'phonenumber':phonenumber,
-              'username':username,
-              'password':password
+        users = {
+            'firstname': firstname,
+            'lastname': lastname,
+            'email': email,
+            'phonenumber': phonenumber,
+            'username': username,
+            'password': password
 
-         }
-         query = """INSERT INTO users(firstname,lastname,email,phonenumber,username,password)
-          VALUES(%s, %s, %s, %s ,%s ,%s)"""
-         cur=self.db.cursor()
-         cur.execute(query,users)
-         self.db.commit()
+        }
+        query = """INSERT INTO users(firstname,lastname,email,phonenumber,username,password)
+          VALUES(%(firstname)s, %(lastname)s,%(email)s, %(phonenumber)s ,%(username)s ,%(password)s)"""
+        cur = self.db.cursor()
+        cur.execute(query, users)
+        self.db.commit()
+        return users
+
+    def fetch_user(self):
+        cur = self.db.cursor()
+        username = request.json.get('username')
+        password = request.json.get('password')
+        cur.execute("""SELECT * FROM users WHERE username = '%s'""" % (username))
+        data = cur.fetchone()
+        value = list(data)
+        if password == value[6]:
+            return True
+        return False
+
+    def verify_membership(self):
+        cur = self.db.cursor()
+        cur.execute("""SELECT * FROM users WHERE username = '%s'""" % (username))
+        data = cur.fetchone()
+        value = list(data)
+        if password == value[6]:
+            return True
+        return False
 
 
-         
