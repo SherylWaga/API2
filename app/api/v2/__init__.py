2667811329a76fbbd896508144e223f41048175d
SherylWaga <sherylwaga@gmail.com>
2018-12-12 14:48:12 +0300

    [Finish #162536855]finish  creation of instance

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from .views.user_views import Registration, Login
 
+from .views.user_views import Registration, Login 
+from .views.instance_views import Create_incident
 version_2 = Blueprint('api_v2', __name__, url_prefix='/api/v2')
 api = Api(version_2)
 
 api.add_resource(Registration, '/users')
-api.add_resource(Login, '/users/login') 
+api.add_resource(Login, '/users/login')
+api.add_resource(Create_incident, '/incidents') 
